e1b1299f2f8a503027d84dfc785811341720ab67
GOPI232 <144507772+GOPI232@users.noreply.github.com>
2023-10-04 19:48:09 +0530

    Add files via upload

--- LeapYear.py
+++ LeapYear.py
@@ -1,6 +1,14 @@
-year = int(input("Enter a year: "))
-if (year % 4 == 0 and year % 100 != 0)     or (year % 400 == 0):
-    print(f"{year} is a leap year.")
+"""a program that determine whether a year entered by the user is a leap year or not using ifelif-else statements"""
+
+
+
+print("FINDING LEAP YEAR OR NOT\n")
+print("************************\n")
+year = int(input("Enter the year : "))
+if(year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+   print("Entered year is a leap year..")
 else:
-    print(f"{year} is not a leap year.")
-  
+  print("Entered year is not a leap year..")
+
+
+           
